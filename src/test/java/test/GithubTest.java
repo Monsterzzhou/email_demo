47fa9cbc2cd1df38ea9a128c7406f1d720410c2b
mrzhou <mrzhou@email.com>
2020-12-04 19:17:24 +0800

    添加测试GitHub

---
+++ src/test/java/test/GithubTest.java
@@ -0,0 +1,13 @@
+package test;
+
+/**
+ * @author 周达
+ * @description GithubTest
+ * @annotation:
+ * @since 2020/12/4 19:14
+ */
+public class GithubTest {
+    public static void main(String[] args) {
+        System.out.println("GitHub测试");
+    }
+}
